8d41ebd8a32c217dfc644592adbbaad6fdd2fa74
Giovanni Bajo <rasky@develer.com>
2012-05-05 00:48:12 +0200

    Add copyright banners

--- src/dnssec-openssl.c
+++ src/dnssec-openssl.c
@@ -1,3 +1,19 @@
+/* dnssec-openssl.c is Copyright (c) 2012 Giovanni Bajo <rasky@develer.com>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; version 2 dated June, 1991, or
+   (at your option) version 3 dated 29 June, 2007.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <string.h>
 #include "dnsmasq.h"
 #include "dnssec-crypto.h"
