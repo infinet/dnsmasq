d310ab7ecbffce79d3d90debba621e0222f9bced
Matthias Andree <matthias.andree@gmx.de>
2014-12-27 15:36:38 +0000

    Fix build failure in new inotify code on BSD.

--- src/inotify.c
+++ src/inotify.c
@@ -15,9 +15,9 @@
 */
 
 #include "dnsmasq.h"
-#include <sys/inotify.h>
-
 #ifdef HAVE_LINUX_NETWORK
+
+#include <sys/inotify.h>
 
 /* the strategy is to set a inotify on the directories containing
    resolv files, for any files in the directory which are close-write 
