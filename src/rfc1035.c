infinet/dnsmasq

commit b8f16556d36924cd8dc7663cb4129d7b1f3fc2be

Tweaks to previous, DNS label charset commit.

src/rfc1035.c
@@ -20,7 +20,7 @@ int extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
 		 char *name, int isExtract, int extrabytes)
 {
   unsigned char *cp = (unsigned char *)name, *p = *pp, *p1 = NULL;
-  unsigned int j, l, hops = 0;
+  unsigned int j, l, namelen = 0, hops = 0;
   int retvalue = 1;
   
   if (isExtract)
@@ -94,9 +94,15 @@ int extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
 	    count = 256;
 	  digs = ((count-1)>>2)+1;
 	  
-	  /* output is \[x<hex>/siz]. which is digs+9 chars */
-	  if (cp - (unsigned char *)name + digs + 9 >= MAXDNAME)
+	  /* output is \[x<hex>/siz]. which is digs+6/7/8 chars */
+	  namelen += digs+6;
+	  if (count > 9)
+	    namelen++;
+	  if (count > 99)
+	    namelen++;
+	  if (namelen+1 >= MAXDNAME)
 	    return 0;
+
 	  if (!CHECK_LEN(header, p, plen, (count-1)>>3))
 	    return 0;
 
@@ -119,7 +125,8 @@ int extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
 	}
       else 
 	{ /* label_type = 0 -> label. */
-	  if (cp - (unsigned char *)name + l + 1 >= MAXDNAME)
+	  namelen += l;
+	  if (namelen+1 >= MAXDNAME)
 	    return 0;
 	  if (!CHECK_LEN(header, p, plen, l))
 	    return 0;
@@ -132,8 +139,12 @@ int extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
 		if (option_bool(OPT_DNSSEC_VALID))
 		  {
 		    if (c == 0 || c == '.' || c == NAME_ESCAPE)
-		      *cp++ = NAME_ESCAPE;
-		    *cp++ = c;
+		      {
+			*cp++ = NAME_ESCAPE;
+			*cp++ = c+1;
+		      }
+		    else
+		      *cp++ = c; 
 		  }
 		else
 #endif
@@ -155,7 +166,7 @@ int extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
 		      c1 += 'a' - 'A';
 #ifdef HAVE_DNSSEC
 		    if (option_bool(OPT_DNSSEC_VALID) && c1 == NAME_ESCAPE)
-		      c1 = *cp++;
+		      c1 = (*cp++)-1;
 #endif
 		    
 		    if (c2 >= 'A' && c2 <= 'Z')

src/dns-protocol.h
@@ -144,5 +144,9 @@ struct dns_header {
   (!CHECK_LEN(header, pp, plen, len) ? 0 : (((pp) += (len)), 1))
 
 /* Escape character in our presentation format for names.
-   Cannot be '.' or /000 and must be !isprint() */
+   Cannot be '.' or /000 and must be !isprint().
+   Note that escaped chars are stored as
+   <NAME_ESCAPE> <orig-char+1>
+   to ensure that the escaped form of /000 doesn't include /000
+*/
 #define NAME_ESCAPE 1

src/dnssec.c
@@ -341,9 +341,11 @@ static int to_wire(char *name)
 	if (*p >= 'A' && *p <= 'Z')
 	  *p = *p - 'A' + 'a';
 	else if (*p == NAME_ESCAPE)
-	  for (q = p; *q; q++)
+	  {
+	    for (q = p; *q; q++)
 	      *q = *(q+1);
-	       
+	    (*p)--;
+	  }
       term = *p;
       
       if ((len = p - l) != 0)
@@ -376,7 +378,8 @@ static void from_wire(char *name)
 	  {
 	    memmove(p+1, p, 1 + last - p);
 	    len++;
-	    *p++ = NAME_ESCAPE;
+	    *p++ = NAME_ESCAPE; 
+	    (*p)++;
 	  }
 	
       l[len] = '.';

src/util.c
@@ -229,7 +229,7 @@ unsigned char *do_rfc1035_name(unsigned char *p, char *sval)
 	{
 #ifdef HAVE_DNSSEC
 	  if (option_bool(OPT_DNSSEC_VALID) && *sval == NAME_ESCAPE)
-	    *p++ = *(++sval);
+	    *p++ = (*(++sval))-1;
 	  else
 #endif		
 	    *p++ = *sval;

commit 5d07d77e75e0f02bc0a8f6029ffbc8b371fa804e

Fix buffer overflow introduced in 2.73rc6.
Fix off-by-one in code which checks for over-long domain names
in received DNS packets. This enables buffer overflow attacks
which can certainly crash dnsmasq and may allow for arbitrary
code execution. The problem was introduced in commit b8f16556d,
release 2.73rc6, so has not escaped into any stable release.
Note that the off-by-one was in the label length determination,
so the buffer can be overflowed by as many bytes as there are
labels in the name - ie, many.

Thanks to Ron Bowes, who used lcmatuf's afl-fuzz tool to find
the problem.

src/rfc1035.c
@@ -94,8 +94,8 @@ int extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
 	    count = 256;
 	  digs = ((count-1)>>2)+1;
 	  
-	  /* output is \[x<hex>/siz]. which is digs+6/7/8 chars */
-	  namelen += digs+6;
+	  /* output is \[x<hex>/siz]. which is digs+7/8/9 chars */
+	  namelen += digs+7;
 	  if (count > 9)
 	    namelen++;
 	  if (count > 99)
@@ -125,8 +125,8 @@ int extract_name(struct dns_header *header, size_t plen, unsigned char **pp,
 	}
       else 
 	{ /* label_type = 0 -> label. */
-	  namelen += l;
-	  if (namelen+1 >= MAXDNAME)
+	  namelen += l + 1; /* include period */
+	  if (namelen >= MAXDNAME)
 	    return 0;
 	  if (!CHECK_LEN(header, p, plen, l))
 	    return 0;